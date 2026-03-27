00f5d1c17a0f75333b6601b6c0a87ad459e04016
Benvindo <geral@menunoar.pt>
2026-03-27 19:46:45 +0000

    fix: adicionar configs Tailwind e PostCSS para corrigir CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}",
+    "./assets/js/**/*.js",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
